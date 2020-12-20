8cb07fcb91e1a1919e0f8f3d9949f0b754c600d1
Jeff Wong <jeff@richmonkeys.com>
2020-12-21 00:21:00 +0800

    initial

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import Busboy from 'busboy'
+import set from 'lodash/set'
+import get from 'lodash/get'
+import { IncomingMessage } from 'http'
+
+export class BusboyFile {
+  filename?: string
+  data?: Buffer
+  size?: number
+  mimetype?: string
+  encoding?: string
+  ext?: string
+  name?: string
+  path?: string
+  buffer?: Buffer
+  width?: number
+  height?: number
+  constructor(filename?: string, data?: Buffer, size?: number, mimetype?: string, encoding?: string) {
+    this.filename = filename
+    this.data = data
+    this.size = size
+    this.mimetype = mimetype
+    this.encoding = encoding
+  }
+}
+
+const parseBody = (req: IncomingMessage) => new Promise(resolve => {
+  const body = {}
+
+  if (!req.headers['content-type']) {
+    return resolve(undefined)
+  }
+
+  const busboy = new Busboy({
+    headers: req.headers,
+  })
+
+  const setBody = (key: string, value: any) => {
+    if (key?.includes('__proto__')) { return }
+
+    const currentValue = get(body, key)
+
+    if (currentValue === undefined) {
+      set(body, key, value)
+    } else if (Array.isArray(currentValue)) {
+      set(body, key, currentValue.concat(value))
+    } else {
+      set(body, key, [currentValue, value])
+    }
+  }
+
+  busboy.on('file', (fieldname: string, file: NodeJS.ReadableStream, filename: string, encoding: string, mimetype: string) => {
+    if (fieldname?.includes('__proto__')) { return }
+    const chunks: Buffer[] = []
+    let size = 0
+    file.on('data', data => {
+      chunks.push(data)
+      size += data.length
+    })
+    file.on('end', () => {
+      const value = new BusboyFile(filename, Buffer.concat(chunks), size, mimetype, encoding)
+      setBody(fieldname, value)
+    })
+  })
+
+  busboy.on('field', (fieldname: string, value: any, fieldnameTruncated: boolean, valTruncated: boolean, encoding: string, mimetype: string) => {
+    try {
+      value = JSON.parse(value)
+    } catch (e) { }
+    setBody(fieldname, value)
+  })
+
+  busboy.on('finish', () => {
+    resolve(body)
+  })
+
+  req.pipe(busboy)
+})
+
+export default parseBody
